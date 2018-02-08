62e4c4df3ccffa1531c9782cf2174b01e4daebec
HVLLOWe3n <Roman.Synovets@gmail.com>
2018-02-08 20:18:16 +0200

    v.0.11 Fixed a vulnerability leading to the removal of posts from other authors

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.datetime.now)
